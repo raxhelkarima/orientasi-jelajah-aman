f67bc72c70a7711ad046c4506bd20dec1956a5dc
Rachel Karima Abdullah <rachel.karima.abdullah24019@mhs.uingusdur.ac.id>
2026-09-15 10:17:12 +0100

    commit pertama

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -10,8 +10,8 @@ export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? 'light';
-  const colorFromProps = props[theme];
+const theme = useColorScheme() === 'dark' ? 'dark' : 'light';
+const colorFromProps = props[theme];
 
   if (colorFromProps) {
     return colorFromProps;
